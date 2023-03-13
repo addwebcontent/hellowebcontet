e34c10b430edbe3149cb0fe2476b9da81a56ba8e
addwebcontent <shaygaming500@gmail.com>
2023-03-13 20:23:37 +0200

    Second Commit Fixed Js for now

--- script .js
+++
@@ -1,7 +0,0 @@
-const navbarToggle = document.querySelector('.navbar-toggle');
-const navbarLinks = document.querySelector('.navbar-links');
-
-navbarToggle.addEventListener('click', function() {
-  navbarToggle.classList.toggle('active');
-  navbarLinks.classList.toggle('show');
-});
