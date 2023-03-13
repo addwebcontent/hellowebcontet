3024b8a4c75b645fcd6c8d0510ebce76ee232d25
addwebcontent <shaygaming500@gmail.com>
2023-03-13 20:47:57 +0200

    Some changed to learn more skills

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const navbarToggle = document.querySelector('.navbar-toggle');
+			const navbarLinks = document.querySelector('.navbar-links');
+
+			navbarToggle.addEventListener('click', function () {
+				navbarToggle.classList.toggle('active');
+				navbarLinks.classList.toggle('show');
+});
+
